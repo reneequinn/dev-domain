828400a566fa10b55c3b6feea899831f1dee2ef5
reneequinn <reneequinn17@gmail.com>
2020-05-30 19:13:59 +0930

    Added database connectivity

--- public/resources/conndb.php
+++ public/resources/conndb.php
@@ -1,22 +1,21 @@
 <?php
 
-require '../../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->load();
+// require '../../vendor/autoload.php';
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+// $dotenv->load();
 
-$dbHost = getenv('DB_HOST');
-$dbUser = getenv('DB_USER');
-$dbPass = getenv('DB_PASS');
-$dbName = getenv('DB_NAME');
-// Connection variables
-// $db = array(
-//   'host' => 'localhost',
-//   'user' => 'root',
-//   'password' => '',
-//   'dbname' => 'codeclubdb'
-// );
+// $dbHost = getenv('DB_HOST');
+// $dbUser = getenv('DB_USER');
+// $dbPass = getenv('DB_PASS');
+// $dbName = getenv('DB_NAME');
 
-// $link = mysqli_connect($db['host'], $db['user'], $db['password'], $db['dbname'] );
+$url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+
+$dbHost = $url['host'];
+$dbUser = $url['user'];
+$dbPass = $url['pass'];
+$dbName = substr($url['path'], 1);
+
 
 $link = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
 
