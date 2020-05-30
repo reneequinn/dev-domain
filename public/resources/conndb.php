cf6e0d423fa73de231aaabdb5924a91f253bfd43
reneequinn <reneequinn17@gmail.com>
2020-05-30 19:42:29 +0930

    Using phpdotenv

--- public/resources/conndb.php
+++ public/resources/conndb.php
@@ -1,21 +1,13 @@
 <?php
 
-// require '../../vendor/autoload.php';
-// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-// $dotenv->load();
+require '../../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
 
-// $dbHost = getenv('DB_HOST');
-// $dbUser = getenv('DB_USER');
-// $dbPass = getenv('DB_PASS');
-// $dbName = getenv('DB_NAME');
-
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$dbHost = $url["host"];
-$dbUser = $url["user"];
-$dbPass = $url["pass"];
-$dbName = substr($url["path"], 1);
-
+$dbHost = getenv('DB_HOST');
+$dbUser = getenv('DB_USER');
+$dbPass = getenv('DB_PASS');
+$dbName = getenv('DB_NAME');
 
 $link = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
 
